0fcbd831f54fcbd379a3352ba9901a8b729d91c0
chafikidrissisara <chafikidrissisara@gmail.com>
2023-08-15 14:39:21 -0700

    betty cheked

--- shell.c
+++ shell.c
@@ -12,45 +12,46 @@ int main(int ac, char **av, char **env)
 		int byte_readed = 0,  status;
 		pid_t pid;
 		int i = 0;
+
 		while (1)
 		{
+			if (isatty(STDIN_FILENO))
+				write(1, "HELL_SHELL>> ", 13);
+			byte_readed = getline(&buffer, &size_buf, stdin);
+			if (byte_readed == EOF)
+			{
+				if (buffer)
+				{
+					free(buffer);
+					buffer = NULL;
+				}
 				if (isatty(STDIN_FILENO))
-						write(1, "HELL_SHELL>> ", 13);
-				byte_readed = getline(&buffer, &size_buf, stdin);
-				if (byte_readed == EOF)
-				{
-						if (buffer)
-						{
-								free(buffer);
-								buffer = NULL;
+					write(1, "\n", 1);
+				free(buffer);
+				exit(0);
+			}
+			else if (*buffer == '\n')
+				free(buffer);
+			else
+			{
+				buffer[strlen(buffer) - 1] = '\0';
+				char **tokens = split_buffer(buffer);
 
-						}
-						if (isatty(STDIN_FILENO))
-								write(1, "\n", 1);
-						free(buffer);
-						exit(0);
-				}
-				else if (*buffer == '\n')
-						free(buffer);
-				else
+				free(buffer);
+				pid = fork();
+				if (pid == 0)
 				{
-						buffer[strlen(buffer) - 1] = '\0';
-						char **tokens = split_buffer(buffer);
-						free(buffer);
-						pid = fork();
-						if (pid == 0)
-						{
-								if (execve(tokens[0], tokens, env) == -1)
-								{
-										printf("%s: No such file or directory\n", av[0]);
-										exit(EXIT_FAILURE);
-								}
-						}
-						else
-								wait(&status);
-						i = 0;
-						free(tokens);
+					if (execve(tokens[0], tokens, env) == -1)
+					{
+						printf("%s: No such file or directory\n", av[0]);
+						exit(EXIT_FAILURE);
+					}
 				}
+				else
+					wait(&status);
+				i = 0;
+				free(tokens);
+			}
 		}
 		return (0);
 }
