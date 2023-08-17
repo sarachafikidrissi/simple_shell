7ca86f7845d2ce7831783e1ce2f9703ecc2a784f
Hichammayat <hichammayat07@gmail.com>
2023-08-17 08:13:07 -0700

    commit

---
+++ _strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: 1 if the strings are the same, 0 otherwise.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	unsigned int i = 0;
+
+	while (s1[i])
+	{
+		if (s1[i] != s2[i])
+		return (0);
+		i++;
+	}
+	return (1);
+}
