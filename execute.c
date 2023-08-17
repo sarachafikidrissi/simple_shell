1f594ad73b3a6649d94ebf1feeee15d27c0db9c4
Hichammayat <hichammayat07@gmail.com>
2023-08-17 09:58:24 -0700

    commit

--- execute.c
+++ execute.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * execute - execution of the command.
+ * @buffer:  pointer to a string.
+ * @env: double pointer to an array of env.
+ */
+
 void execute(char *buffer, char **env)
 {
 	char **tokens = NULL;
@@ -10,36 +16,35 @@ void execute(char *buffer, char **env)
 	buffer[_strlen(buffer) - 1] = '\0';
 	tokens = split_buffer(buffer);
 
-				pid = fork();
-				if (pid == 0)
+	pid = fork();
+	if (pid == 0)
+	{
+		if (stat(tokens[0], &st) == 0)
+		{
+			if (execve(tokens[0], tokens, env) == -1)
+			{
+				perror(tokens[0]);
+				free_exit(tokens);
+			}
+		}
+		else
+		{
+			full_path = get_full_path(tokens[0]);
+			if (full_path)
+			{
+				if (execve(full_path, tokens, env) == -1)
 				{
-					if (stat(tokens[0], &st) == 0)
-					{
-						if (execve(tokens[0], tokens, env) == -1)
-						{
-							perror(tokens[0]);
-							free_exit(tokens);
-						}
-					}
-					else
-					{
-						full_path = get_full_path(tokens[0]);
-						if (full_path)
-						{
-							if (execve(full_path, tokens, env) == -1)
-							{
-								perror("Error");
-								free(full_path);
-								free_command(tokens);
-							}
-						}
-						else
-							printf("%s: not found\n", tokens[0]);
-						exit(0);
-					}
+					perror("Error");
+					free(full_path);
+					free_command(tokens);
 				}
-				else
-					wait(&status);
-				free(tokens);
-		
+			}
+			else
+			printf("%s: not found\n", tokens[0]);
+			exit(0);
+		}
+	}
+	else
+	wait(&status);
+	free(tokens);
 }
