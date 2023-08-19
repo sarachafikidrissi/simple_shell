d837fe46cf2a1aec9d59ba4f7daaf90fe2a88f8e
chafikidrissisara <chafikidrissisara@gmail.com>
2023-08-18 20:43:03 -0700

    fixing some errors

--- shell.c
+++ shell.c
@@ -21,13 +21,12 @@ int main(int ac, char **av, char **env)
 		byte_readed = getline(&buffer, &size_buf, stdin);
 		if (byte_readed == EOF)
 			handle_EOF(buffer);
-		else if (*buffer == '\n')
+		else if (*buffer == '\n' || strspn(buffer, " \t\r\n") == strlen(buffer))
 		{
 			continue;
 		}
 		else
 			execute(buffer, env);
-		free(buffer);
 		buffer = NULL;
 	}
 	return (0);
